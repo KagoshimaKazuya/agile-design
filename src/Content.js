b88046b60ff2502d90283c2d93b8814f8e1387d6
久我 鉄郎 <N26193@tes.ns-sol.co.jp>
2024-11-21 16:16:19 +0900

    reader

--- src/Content.js
+++ src/Content.js
@@ -1,8 +1,38 @@
+import page01 from './img/01.png';
+import './Content.css'
+
 export const Content = () => {
     return(
         <>
-            <div>
-                <h1>内容のページ</h1>
+            <div className="main">
+                <div className="leftNavigation">
+                    <p>サブメニュー</p>
+
+                </div>
+                <div className="content">
+
+
+                    <div className="contentheader">
+                        <button><font size="7">×</font></button>
+                    </div>
+
+                    <div className="reader">
+                        <img src={page01} alt="img"/>
+                    </div>
+                    <footer>
+                        <div className="arrowgramleft">
+                            <button>◀</button>
+                        </div>
+
+                        <div className="pages">
+                            <p>ページ数</p>
+                        </div>
+
+                        <div className="arrowgramright">
+                        <button>▶</button>
+                        </div>
+                    </footer>
+                </div>
             </div>
         </>
     );
